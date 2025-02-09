28dcedecad5f6cb1c8c21dce804a3240c7c64d73
Nekit <nick.misiura@yandex.ru>
2025-02-09 14:57:55 +0300

    init create-next-app@14.2.23

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-[100vh] h-fit">
+        <p>Nothing here</p>
+    </div>
+  );
+}
